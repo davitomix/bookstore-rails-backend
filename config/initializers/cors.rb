9af32d85654134f2a95acf7df168311891376a1a
David Elì <davidelimg1@gmail.com>
2020-08-28 13:50:10 -0500

    removed sqlit replaced to postgres

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-      origins ENV['API_URL'] 
+      origins '#{ENV['API_URL']}' 
     # origins 'https://frosty-engelbart-d94f71.netlify.app/'
 
       headers: :any,
