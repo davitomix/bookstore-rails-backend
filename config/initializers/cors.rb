2577b21f321de1e1250a08900dd014d7341ef631
David Elì <davidelimg1@gmail.com>
2020-08-28 13:59:16 -0500

    Reverted to original path

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://frosty-engelbart-d94f71.netlify.app/'
+    origins '*'
 
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
